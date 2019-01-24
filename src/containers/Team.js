0aca08684d8e32c0a27cc636d8147ce698c5e5dc
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-01-24 17:18:59 +0100

    feat(chikusho): /team base content

--- src/containers/Team.js
+++ src/containers/Team.js
@@ -9,14 +9,15 @@ class Team extends Component {
   render() {
     console.log(this.state.team);
     return (
-      <div>
+      <div id="team-container">
         <h1>Team</h1>
         {this.state.team
         .map(member => 
         <div key={member.id}>
           <div style={{backgroundImage: "url(" + member.picUrl + ")"}}></div>
           <p>{member.name}</p>
-          <div></div>
+          <div style={{backgroundImage: "url(github.svg)"}}></div>  
+          <div style={{backgroundImage: "url(linkedin.svg)"}}></div>
           <p>{member.info}</p>
         </div>)}
       </div>
