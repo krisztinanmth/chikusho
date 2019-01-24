f23aedf16e7b1c1a53077fa679b0fc4d682ea978
Gyarmati Ádám <gyarmatiaadaam@gmail.com>
2019-01-24 15:38:00 +0100

    feat(chikusho): teamContent and pics add

---
+++ src/content/TeamContent.js
@@ -0,0 +1,32 @@
+const teamContent = [
+  {
+    picUrl: '../pic/kriszta.png',
+    name: 'Németh Krisztina',
+    github: 'https://github.com/krisztinanmth',
+    linkedIn: 'https://www.linkedin.com/in/krisztina-nemeth-48296041/',
+    info: 'I like coding and having fun.'
+  },
+  {
+    picUrl: '../pic/nori.png',
+    name: 'Puskás Nóra',
+    github: 'https://github.com/norlandia',
+    linkedIn: 'https://www.linkedin.com/in/norapuskas/',
+    info: ''
+  },
+  {
+    picUrl: '../pic/laz.png',
+    name: 'Daru László',
+    github: 'https://github.com/ldaruu',
+    linkedIn: 'https://www.linkedin.com/in/laszlodaru/',
+    info: ''
+  },
+  {
+    picUrl: '../pic/adam.png',
+    name: 'Gyarmati Ádám',
+    github: 'https://github.com/adihipo',
+    linkedIn: 'https://www.linkedin.com/in/%C3%A1d%C3%A1m-gyarmati-46a852135/',
+    info: 'I love puzzles, dogs and dog puzzles.'
+  }
+] 
+
+export default teamContent;
