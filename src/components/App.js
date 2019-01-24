97a48cb6064a5268e9683104fa01314b8e478c81
Laszlo Daru <daruu@hotmail.com>
2019-01-24 16:38:27 +0100

    feat(Rules.js)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Team from '../containers/Team';
+import Rules from '../containers/Rules'
 
 const App = () => {
   return (
     <Router>
+      <div>
       <Route exact path="/team" component={Team} />
+      <Route  path="/rules" component={Rules}/>
+      </div>
     </Router>
   );
 };
--- src/content/rules.js
+++ src/content/rules.js
@@ -1,8 +1,16 @@
 const rules = [
-    {
-        1: 'Any live cell with fewer than two live neighbors dies, as if by underpopulation.',
-        2: 'Any live cell with two or three live neighbors lives on to the next generation.',
-        3: 'Any live cell with more than three live neighbors dies, as if by overpopulation.',
-        4: 'Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.',
-    }
+    { rule: 'Any live cell with fewer than two live neighbors dies, as if by underpopulation.',
+      id: 1   },
+        
+    {rule: 'Any live cell with two or three live neighbors lives on to the next generation.', 
+     id: 2   },
+
+    { rule: 'Any live cell with more than three live neighbors dies, as if by overpopulation.',
+      id: 3  },
+
+    { rule: 'Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.',
+      id: 4  },
+ 
 ];
+
+export default rules
