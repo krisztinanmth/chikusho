613ab0f7c9f0b310efad2726bfc8084a5a2cc69e
Nora <norapuskas@yahoo.com>
2019-01-24 15:35:08 +0100

    feat(Board.js, BoardContainer.js): create components

---
+++ src/containers/BorardContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Board from '../components/Board';
+
+class BoardContainer extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Board />
+      </div>
+    );
+  }
+}
+
+export default BoardContainer;
