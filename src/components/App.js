db6523b33a85863bddf0cb025f5df3009efed58a
Nora <norapuskas@yahoo.com>
2019-01-24 14:46:24 +0100

    refact(Team.js): move Team.js to containers folder, change it to statefull component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Team from './Team';
+import Team from '../containers/Team';
 
 const App = () => {
   return (
